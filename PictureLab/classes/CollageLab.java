b0854953188427741dbf146ba8abe98307c54cf8
ErixKim <edkim@stu.naperville203.org>
2016-01-21 10:29:45 -0600

    1/21/2016

--- PictureLab/classes/CollageLab.java
+++ PictureLab/classes/CollageLab.java
@@ -3,11 +3,23 @@ public class CollageLab
     public static void main(String[] args)
     {
         Picture picture = new Picture("blue-mark.jpg");
-        for (int i = 0; i < 6; i++)
+        Picture canvas = new Picture(480,640);
+        canvas.cropAndCopy(picture,0,480,0,640,0,0);
+        for (int i = 0; i < 1; i++)
         {
-            Picture newPicture = picture.scaleByHalf();
-            picture.cropAndCopy(newPicture,100,200,100,200,300,300);
+            Picture newPicture = canvas.scaleByHalf();
+            if (i % 2 != 0)
+            {
+                newPicture.grayscale();
+            }
+            else
+            {
+                newPicture.negate();
+            }
+            canvas.cropAndCopy(newPicture,0,240,0,320,0,0);
         }
-        picture.explore();
+        canvas.mirrorVertical();
+        canvas.mirrorHorizontal();
+        canvas.explore();
     }
 }
